27a1fa2c44cf788fb6febf223d1fb547e0185a8a
masaditya <aeka489@gmail.com>
2020-06-04 12:41:47 +0700

    fix(login) : adding illustration login screen

--- src/container/login/LoginPages.jsx
+++ src/container/login/LoginPages.jsx
@@ -3,6 +3,8 @@ import { RootContext } from "../../context/Context";
 import { Login } from "../../context/actions/actions";
 import { toast } from "react-toastify";
 import { LOGIN_SUCCESS } from "../../context/actionTypes";
+import { Form, Input, Button } from "antd";
+import login_illustraion from "../../assets/undraw_authentication_fsn5.svg";
 
 export const LoginPages = () => {
   const { dispatch } = useContext(RootContext);
@@ -28,20 +30,64 @@ export const LoginPages = () => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <input
-        onChange={(e) => setUsername(e.target.value)}
-        value={username}
-        type="text"
-        name="username"
-      />
-      <input
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-        type="password"
-        name="password"
-      />
-      <button type="submit">Login</button>
-    </form>
+    <div className="login-screen">
+      <div className="illustration-container">
+        <img className="login-illustration" src={login_illustraion} alt="" />
+      </div>
+      <div className="login-container">
+        <Form className="m-auto" onSubmit={submitHandler}>
+          <h1 className="greeting-text">
+            Hello, <br /> Welcome Back!
+          </h1>
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+              type="text"
+              name="username"
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              type="password"
+              name="password"
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+
+        {/* <form className="m-auto" onSubmit={submitHandler}>
+          <input
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            type="text"
+            name="username"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            type="password"
+            name="password"
+          />
+          <button type="submit">Login</button>
+        </form> */}
+      </div>
+    </div>
   );
 };
